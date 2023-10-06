6ca2dd6018ca6623bb4f7b94832b161b476c011e
Arie Twigt <twigt.arie@gmail.com>
2023-10-06 16:47:52 +0200

    added the dunder main

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from rdw_functions.import_functions import import_cars_by_brand
 from rdw_functions.export_functions import export_cars_list_to_csv
 from tqdm import tqdm
 
-# specify the brand
-selected_brand =  input("Insert a car brand:\n")
 
-# convert to a list
-selected_brands_list = selected_brand.split("+")
+#
+if __name__ == "__main__":
 
-for brand in tqdm(selected_brands_list):
-    # get the cars from the RDW
-    cars_list = import_cars_by_brand(brand)
+    # specify the brand
+    selected_brand =  input("Insert a car brand:\n")
 
-    # check if the list is not empty
-    if len(cars_list) == 0:
-        continue
+    # convert to a list
+    selected_brands_list = selected_brand.split("+")
 
-    # export the to a csv
-    export_cars_list_to_csv(cars_list, brand)
+    for brand in tqdm(selected_brands_list):
+        # get the cars from the RDW
+        cars_list = import_cars_by_brand(brand)
+
+        # check if the list is not empty
+        if len(cars_list) == 0:
+            continue
+
+        # export the to a csv
+        export_cars_list_to_csv(cars_list, brand)
 
---
+++ not_main.py
@@ -0,0 +1,6 @@
+
+__name__
+
+import main
+
+
